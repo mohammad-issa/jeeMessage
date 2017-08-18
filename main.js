79a6ef85365f322c94765141d22ad51e0ed79b29
Mohammad issa <asali@mxd3d.com>
2017-08-18 17:24:54 +0300

    rename

--- main.js
+++ main.js
@@ -7,10 +7,10 @@ $(document).ready(function(){
 	});
 
 	$('#success').click(function (argument) {
-		jeeMessages.addMsg('warning','Lorem Ipsum is simply dummy text of the printing','typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum');
+		jeeMessages.jeeAdd('warning','Lorem Ipsum is simply dummy text of the printing','typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum');
 	});
 	$('#success2').click(function (argument) {
-		jeeMessages.addMsg();
+		jeeMessages.jeeAdd();
 	});
 
 
